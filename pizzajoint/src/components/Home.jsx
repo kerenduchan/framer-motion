63d9ef0c51d4d112191d8b2830d8a6686f4f9f90
Keren Duchan <keren.duchan@gmail.com>
2023-09-27 22:01:04 +0300

    framer-motion basics lesson-3

--- pizzajoint/src/components/Home.jsx
+++ pizzajoint/src/components/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 export function Home() {
     return (
-        <div className="home container">
+        <motion.div animate={{}} className="home container">
             <h2>Welcome to Pizza Joint</h2>
             <Link to="/base">
-                <button>Create Your Pizza</button>
+                <motion.button animate={{}}>Create Your Pizza</motion.button>
             </Link>
-        </div>
+        </motion.div>
     )
 }
